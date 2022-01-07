1be7bc803131798311edf1a3987707ecb0511ab5
Agustin Ranieri <aguseranieri@gmail.com>
2022-01-07 15:29:00 -0300

    breaking change makefile tests

---
+++ tests/settings.mk
@@ -0,0 +1,25 @@
+# =========================[ Compilation variables ]========================= #
+
+# Test project name
+TEST_NAME=$(shell cd . && pwd | xargs basename)
+
+# Original project path
+PROJ_PATH=../project
+
+# Original project main function source file path (will be excluded from tests)
+PROJ_MAIN=$(PROJ_PATH)/src/main.c
+
+# Testing library
+TEST_LIB=cspecs
+
+# All libraries except 'readline', custom libraries' paths and compiled flags
+# will be imported from PROJ_PATH
+
+# ==========================[ Profiling variables ]========================== #
+
+# Arguments when executing with start, memcheck or helgrind
+TEST_ARGS=
+
+# Valgrind flags
+TEST_MEMCHECK_FLAGS=--track-origins=yes --log-file="memcheck-$(NAME).log"
+TEST_HELGRIND_FLAGS=--log-file="helgrind-$(NAME).log"
