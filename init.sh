2044404f2b8a4c7e9199c4e87d1fce91b5e14c04
Agustín Ranieri <aguseranieri@gmail.com>
2022-02-10 23:27:11 -0300

    Delete init.sh

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-REPONAME="RaniAgus/so-project-template"
-TEMPLATENAME="${1:?}"
-
-read -p "$TEMPLATENAME name: ($TEMPLATENAME) " PROJNAME
-
-wget -qO- https://github.com/$REPONAME/releases/latest/download/$TEMPLATENAME.tar.gz\
- | tar -xvzf - --strip-components 1 --one-top-level=${PROJNAME:-$TEMPLATENAME}
