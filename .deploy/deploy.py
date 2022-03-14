a038d932c7bc08976a141d4eb676ae5e11fa9e7e
Agustin Ranieri <aguseranieri@gmail.com>
2022-03-13 22:38:39 -0300

    actualizo deploy.py

--- .deploy/deploy.py
+++ .deploy/deploy.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
-import os, sys
+import sys
+import os
 
-def __main__(dir):
-  os.chdir(dir)
-  with open('./makefile', 'r') as file:
-    for line in file:
-      parse_line(line.strip())
 
-def parse_line(line):
-  if line.startswith('include') and not 'settings' in line:
-    include(line)
-  else:
-    print(line)
+def main(dir: str) -> None:
+    os.chdir(dir)
+    with open('makefile', 'r') as file:
+        for line in file:
+            parse_line(line.strip())
 
-def include(line):
-  _, filename = line.split(' ')
-  with open(filename, 'r') as file:
-    print(file.read())
 
-__main__(sys.argv[1])
+def parse_line(line: str) -> None:
+    if line.startswith('include ../'):
+        include(line)
+    else:
+        print(line)
+
+
+def include(line: str) -> None:
+    _, filename = line.split(' ')
+    with open(filename, 'r') as file:
+        print(file.read())
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
