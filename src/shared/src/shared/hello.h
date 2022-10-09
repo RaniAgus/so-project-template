2f3a728eb122ae45a1028da3968a17ddfe08a555
Agustin Ranieri <aguseranieri@gmail.com>
2022-10-09 15:41:30 -0300

    agrego headers de libs

---
+++ src/shared/src/shared/hello.h
@@ -0,0 +1,13 @@
+#ifndef SHARED_HELLO_H_
+#define SHARED_HELLO_H_
+
+    #include <stdlib.h>
+    #include <stdio.h>
+
+    /**
+    * @NAME: say_hello
+    * @DESC: Imprime un saludo al nombre que se pase por parámetro por consola.
+    */
+    int say_hello(char* who);
+
+#endif
