b99eabe123d189b2e05f3bcc1f06f13cbcba8705
Agustin Ranieri <aguseranieri@gmail.com>
2023-03-01 16:54:09 -0300

    actualizo spec inicial

--- src/static/spec/example_spec.c
+++ src/static/spec/example_spec.c
@@ -3,23 +3,12 @@
 #include <cspecs/cspec.h>
 
 context (example) {
+    bool la_verdad = true;
 
     describe("Hello world") {
 
-        it("true should be true") {
-            should_bool(true) be equal to(true);
-        } end
-
-        it("true shouldn't be false") {
-            should_bool(true) not be equal to(false);
-        } end
-
-        it("this test will fail because 10 is not equal to 11") {
-            should_int(10) be equal to(11);
-        } end
-
-        skip("this test will fail because \"Hello\" is not \"Bye\"") {
-            should_string("Hello") be equal to("Bye");
+        it("la_verdad should be true") {
+            should_bool(la_verdad) be equal to(true);
         } end
 
     } end
