0622e4b1893764ccbf683bbc77ff37bbe95e2551
Agustin Ranieri <aguseranieri@gmail.com>
2023-03-29 00:16:48 -0300

    quito log files por defecto

--- src/project/settings.mk
+++ src/project/settings.mk
@@ -13,8 +13,8 @@ CRELEASE=-O3 -Wall -DNDEBUG
 ARGS=
 
 # Valgrind flags
-MEMCHECK_FLAGS=--track-origins=yes --log-file="memcheck.log"
-HELGRIND_FLAGS=--log-file="helgrind.log"
+MEMCHECK_FLAGS=--track-origins=yes
+HELGRIND_FLAGS=
 
 # Source files (*.c) to be excluded from tests compilation
 TEST_EXCLUDE=src/main.c
