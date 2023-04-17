538c8f2afe98325faed5f62b78131e2ebe165f25
Agustin Ranieri <aguseranieri@gmail.com>
2023-04-17 15:02:32 -0300

    fix: deteccion de errores en vscode

--- src/static/settings.mk
+++ src/static/settings.mk
@@ -6,7 +6,7 @@ SHARED_LIBPATHS=
 STATIC_LIBPATHS=
 
 # Compiler flags
-CDEBUG=-g -Wall -DDEBUG
+CDEBUG=-g -Wall -DDEBUG -fdiagnostics-color=always
 CRELEASE=-O3 -Wall -DNDEBUG
 
 # Source files (*.c) to be excluded from tests compilation
