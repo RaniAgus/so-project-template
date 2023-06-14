26cf3b5863fee9ca58651ef2893adab18ff06ac8
Agustin Ranieri <aguseranieri@gmail.com>
2023-06-14 15:01:18 -0300

    agrego flag -fcommon por default

--- src/shared/settings.mk
+++ src/shared/settings.mk
@@ -6,8 +6,8 @@ SHARED_LIBPATHS=
 STATIC_LIBPATHS=
 
 # Compiler flags
-CDEBUG=-g -Wall -DDEBUG -fdiagnostics-color=always
-CRELEASE=-O3 -Wall -DNDEBUG
+CDEBUG=-g -Wall -DDEBUG -fcommon -fdiagnostics-color=always
+CRELEASE=-O3 -Wall -DNDEBUG -fcommon
 
 # Source files (*.c) to be excluded from tests compilation
 TEST_EXCLUDE=
